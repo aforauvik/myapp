d9ed5d071aa5be13d6cfa3833ce79b1f8563f295
Auvik <email@auvik.me>
2024-03-16 22:28:28 -0400

    updates

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -19,8 +19,8 @@ function Card() {
 							</a>
 						</span>
 						. I like to design human-centered, modern, and most importantly
-						functional products. I believe me != user and design decisions
-						should be data-driven.
+						functional products. I believe design decisions should be
+						data-driven.
 					</p>
 				</div>
 
