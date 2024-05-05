91905237bea87b3040a4f2b7db0c781d3cb3a5fb
Auvik <email@auvik.me>
2024-05-05 00:42:20 -0400

    social icons

--- src/Card/Footer.js
+++ src/Card/Footer.js
@@ -17,16 +17,32 @@ function Footer() {
 			<div className={styles.footer}>
 				<div className={styles.divider}></div>
 				<div style={iconStyle}>
-					<a target="blank" href="https://dribbble.com/auvik">
+					<a
+						className={styles.footerIcons}
+						target="blank"
+						href="https://dribbble.com/auvik"
+					>
 						<img height={24} src="/dribbble-icon.svg" alt="" srcset="" />
 					</a>
-					<a target="blank" href="https://www.instagram.com/auvik">
+					<a
+						className={styles.footerIcons}
+						target="blank"
+						href="https://www.instagram.com/auvik"
+					>
 						<img height={24} src="/Instagram-icon.svg" alt="" srcset="" />
 					</a>
-					<a target="blank" href="https://twitter.com/auvik">
+					<a
+						className={styles.footerIcons}
+						target="blank"
+						href="https://twitter.com/auvik"
+					>
 						<img height={24} src="/Twitter.svg" alt="" srcset="" />{" "}
 					</a>
-					<a target="blank" href="mailto:email@auvik.me">
+					<a
+						className={styles.footerIcons}
+						target="blank"
+						href="mailto:email@auvik.me"
+					>
 						<img height={24} src="/mailto-icon.svg" alt="" srcset="" />{" "}
 					</a>
 				</div>
